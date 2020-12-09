cdccb35c26772298fb4116f5b88520fe68658581
elVictor <39276857+Elvictor01@users.noreply.github.com>
2020-12-09 08:57:12 +0100

    Update taxonomicClass.py

--- taxonomicClass.py
+++ taxonomicClass.py
@@ -2,8 +2,9 @@ import sys
 import os
 import io
 import qiime2.plugins
+import pandas as pd
 
-from qiime2.plugins import metadata, taxa, composition
+from qiime2.plugins import metadata, taxa, composition, feature_classifier
     
 workdir='./'
 
@@ -15,20 +16,30 @@ def taxaClass( reads):
 
     taxonomy = feature_classifier.methods.classify_sklearn(reads = qiime2.Artifact.load(reads),
                                                        classifier = gg_classifier)
+    taxa_class=taxonomy.classification.save("taxa_class.qza")
+
+
+
+    taxonomy_artifact = qiime2.Artifact.load('taxa_class.qza')
+
+    taxonomy_df = taxonomy_artifact.view(pd.DataFrame)
+    taxonomy_df.to_csv("taxaClass.csv",index= False)
+    
+
 
 
     return taxonomy
 
-    taxonomy_classification = metadata.visualizers.tabulate(taxonomy.classification.view(qiime2.Metadata))
-    taxonomy_classification.visualization
+    #taxonomy_classification = metadata.visualizers.tabulate(taxa_class.view(qiime2.Metadata))
+    #taxonomy_classification.visualization
 
-    taxa_bar_plot = taxa.visualizers.barplot(clean_seq.table, taxonomy.classification, sample_metadata)
+    taxa_bar_plot = taxa.visualizers.barplot(qiime2.Artifact.load(table_seq.qza), taxa_class, sample_metadata)
     taxa_bar_plot.visualization
     
 
 def main():
     "Input is denoised representative sequence"
-    denoise(sys.argv[1])
+    taxaClass(sys.argv[1])
 
 
 main()
